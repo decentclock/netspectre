0a8d545bc1a82834533ee01782508db5e7fe0389
Jules Comte <hello@jules.cx>
2021-03-22 19:20:05 -0400

    create functions

--- client.c
+++ client.c
@@ -3,20 +3,31 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 #define PORT 8080
 
 int main()
 {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "\x00";
     sock = socket(AF_INET, SOCK_STREAM, 0);
-
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-
     inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    send(sock, hello, strlen(hello), 0);
+    int val = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    if (val == -1)
+        printf("ERROR CODE: %i\n", errno);
+
+    char hello[2] = {'\x01','\x00'};
+    int characters_sent = send(sock, &hello, 2, 0);
+
+    printf("I just sent %i characters\n", characters_sent);
+
+    char buffer[1] = {0};
+    valread = read(sock, buffer, 1);
+    printf("%i\n", buffer[0]);
+
+    shutdown(sock, 2);
     return 0;
 }
--- server.c
+++ server.c
@@ -11,26 +11,46 @@ int main()
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char buffer[1] = {0};
+
+    char buffer[2] = {0};
     char flag = '\x00';
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
+
     bind(server_fd, (struct sockaddr *)&address, sizeof(address));
     listen(server_fd, 3);
-    int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-    int valread = read(new_socket, buffer, 1);
-    printf("%i\n", buffer[0]);
 
-    char bitstream[5] = {0,1,2,3,4};
-    if (buffer[0] < 5)
+    while (1)
     {
-        if (bitstream[buffer[0]])
+        int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        int valread = read(new_socket, buffer, 2);
+
+        if (buffer[0] == '\x00')
         {
-            flag = '\x01';
-            printf("Flag set to true\n");
+            char bitstream[5] = {0,1,2,3,4};
+            if (buffer[1] < 5)
+            {
+                if (bitstream[buffer[1]])
+                {
+                    flag = '\x01';
+                    printf("Flag set to true\n");
+                }
+            }
         }
+        else if (buffer[0] == '\x01')
+        {
+            send(new_socket, &flag, 1, 0);
+        }
+        else if (buffer[0] == '\xFF')
+        {
+            shutdown(new_socket, 2);
+            break;
+        }
+        shutdown(new_socket, 2);
     }
+
+    shutdown(server_fd, 2);
     return 0;
 }
