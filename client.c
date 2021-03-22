8290dd0100f4052ce4a1b27273e8247be4037245
Jules Comte <hello@jules.cx>
2021-03-22 17:29:48 -0400

    leak gadget written

--- client.c
+++ client.c
@@ -9,8 +9,7 @@ int main()
 {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "What the fuck";
-    char buffer[1024] = {0};
+    char *hello = "\x00";
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
     serv_addr.sin_family = AF_INET;
@@ -19,7 +18,5 @@ int main()
     inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
     connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     send(sock, hello, strlen(hello), 0);
-    valread = read(sock, buffer, 1024);
-    printf("%s\n", buffer);
     return 0;
 }
--- server.c
+++ server.c
@@ -11,8 +11,8 @@ int main()
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "I am your father";
+    char buffer[1] = {0};
+    char flag = '\x00';
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
@@ -20,8 +20,17 @@ int main()
     bind(server_fd, (struct sockaddr *)&address, sizeof(address));
     listen(server_fd, 3);
     int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-    int valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
+    int valread = read(new_socket, buffer, 1);
+    printf("%i\n", buffer[0]);
+
+    char bitstream[5] = {0,1,2,3,4};
+    if (buffer[0] < 5)
+    {
+        if (bitstream[buffer[0]])
+        {
+            flag = '\x01';
+            printf("Flag set to true\n");
+        }
+    }
     return 0;
 }
