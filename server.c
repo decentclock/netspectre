b5c916422a1bc8c34517bc0ea412b65f50cdfee6
Michael Zhang <jialiaz3@uci.edu>
2021-03-31 15:43:57 -0400

    add AVX function

--- server.c
+++ server.c
@@ -16,6 +16,10 @@ int main()
     char buffer[2] = {0};
     char flag = '\x00';
 
+    /* Initialize the two argument vectors */
+    __m256 evens = _mm256_set_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
+    __m256 odds = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
@@ -37,6 +41,13 @@ int main()
                 {
                     flag = '\x01';
                     printf("Flag set to true\n");
+
+                    /* Compute the difference between the two vectors */
+                    __m256 result = _mm256_sub_ps(evens, odds);
+
+                    /* Display the elements of the result vector */
+                    float* f = (float*)&result;
+                    printf("%f %f %f %f %f %f %f %f\n", f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7]);
                 }
             }
         }
@@ -55,3 +66,8 @@ int main()
     shutdown(server_fd, 2);
     return 0;
 }
+
+
+/* Please compile server.c on Windows OS with the following command:
+ * gcc -mavx -o server server.c
+ * */
